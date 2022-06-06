27accd3a566b85cbafcf77f0cc1e594d215291d3
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-06-07 00:43:30 +0300

    ht 5

--- src/components/views/MoviesPage.jsx
+++ src/components/views/MoviesPage.jsx
@@ -1,5 +1,79 @@
+import { useState, useEffect } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import imagesAPI from 'components/services/APIRequests';
+
 const MoviesPage = () => {
-  return <h1>MoviesPage</h1>;
+  const { url } = useRouteMatch();
+  const [moviesName, setMoviesName] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [movies, setMovies] = useState(null);
+  const [unmount, setUnmount] = useState(true);
+
+  const handleNameChange = e => {
+    setMoviesName(e.currentTarget.value.toLowerCase());
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (moviesName.trim() === '') {
+      alert('введите имя изображения');
+      return;
+    }
+
+    setUnmount(false);
+  };
+
+  useEffect(() => {
+    if (unmount) {
+      return;
+    }
+
+    imagesAPI
+      .fetchSearchByKeyword(moviesName)
+      .then(movies => {
+        setMovies(movies.results);
+        setStatus('resolved');
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('rejected');
+      });
+
+    return () => {
+      setUnmount(true);
+    };
+  }, [unmount, moviesName]);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <button type="submit">
+          <Link to={`${url}/${moviesName}`}>Найти</Link>
+        </button>
+
+        <input
+          type="text"
+          placeholder="Search movies and photos"
+          onChange={handleNameChange}
+          value={moviesName}
+        />
+      </form>
+      {status === 'rejected' && <h1>{error.massage}</h1>}
+      {status === 'resolved' && (
+        <ul>
+          {movies.map(movie => {
+            return (
+              <li key={movie.id}>
+                <Link to={`${url}/${movie.id}`}>{movie.original_title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default MoviesPage;
